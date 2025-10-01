7b1b903d9fb35f2baeab46decb6f6590d7eab4aa
Filip Hand <filif.reka@gmail.com>
2025-10-01 17:23:24 +0200

    Moved .h files and wget when not there

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if [[ ! -d ./dependencies ]]; then
+    mkdir ./dependencies
+fi
+
+if [[ ! -f ./dependencies/stb_image_write.h ]]; then
+    wget -O ./dependencies/stb_image_write.h https://raw.githubusercontent.com/nothings/stb/refs/heads/master/stb_image_write.h
+fi
+
+if [[ ! -f ./dependencies/stb_image.h ]]; then
+    wget -O ./dependencies/stb_image.h https://raw.githubusercontent.com/nothings/stb/refs/heads/master/stb_image.h
+fi
+
 filename=$1
 result="${filename%.*}"
 
